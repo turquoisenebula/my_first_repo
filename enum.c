ed79ec83712fd798ee1f4ebaf5e2056e9f9cdaef
turquoisenebula <shikhadwivedi2737@gmail.com>
2024-10-21 12:12:13 +0530

    Update enum.c freaure 1

--- enum.c
+++ enum.c
@@ -7,6 +7,4 @@ int main()
     int *p=&n;  //here * declears p as a pointer 
     printf("%i",*p); //here * means go to the memory and show me what's there
     printf("%p",&n); //this prints the address of n
-    
-
 }
